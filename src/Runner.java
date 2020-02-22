66838972be469a2d1099fd2c0741fc7ab6f036b0
malgantual <56133144+malgantual@users.noreply.github.com>
2020-02-21 22:15:47 -0600

    updated Runner + UI

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+
+public class Runner
+{
+	public static void main(String[] args)
+	{
+		UI game = new UI();
+	}
+}
--- src/UI.java
+++ src/UI.java
@@ -1,1 +1,76 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class UI extends JPanel implements MouseListener
+{
+	private String[] titles = new String[]
+			{"the", "testing", "another test", "UI"};
+	private int width = 1920, height = 1080;
+	
+	private JFrame frame;
+	
+	public UI()
+	{
+		super();
+		
+		Dimension size = new Dimension(width, height);
+		setPreferredSize(size);
+		setSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		
+		addMouseListener(this);
+		
+		frame = new JFrame(titles[(int)(Math.random() * titles.length)]); // selects a random title
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+//		frame.setResizable(false);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
