babfe18dd83129a4e34058c2d5509d26cb536215
malgantual <56133144+malgantual@users.noreply.github.com>
2020-02-23 00:04:18 -0600

    proof of concept for graphics
    i guess

--- src/UI.java
+++ src/UI.java
@@ -7,7 +7,12 @@ import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -16,6 +21,7 @@ public class UI extends JPanel implements MouseListener, MouseMotionListener
 	private String[] titles = new String[]
 			{"the", "testing", "another test", "UI"};
 	private int width = 1920, height = 1080;
+	private HashMap<String, BufferedImage> images;
 	
 	private JFrame frame;
 	
@@ -49,6 +55,18 @@ public class UI extends JPanel implements MouseListener, MouseMotionListener
 //		frame.setResizable(false);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		
+		images = new HashMap<String, BufferedImage>();
+		try
+		{
+			images.put("water", ImageIO.read(new File("assets/water.png")));
+			images.put("map", ImageIO.read(new File("assets/us_map.png")));
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		frame.setVisible(true);
 	}
 	
@@ -56,24 +74,32 @@ public class UI extends JPanel implements MouseListener, MouseMotionListener
 	public void paint(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D)g;
+		
+		
+		// temp code (dragging test)
 		if (firstTime)
 		{
 			piece.setLocation(width/2, height/2);
 			firstTime = false;
 		}
 		
-		g2.setColor(Color.white);
-		g2.fillRect(0, 0, width, height);
+		paintBackground(g2);
 		
 		g2.setColor(new Color(132, 232, 182));
 		g2.fill(piece);
+		//
+	}
+	
+	public void paintBackground(Graphics2D g2)
+	{
+		g2.drawImage(images.get("water"), 0, 0, null);
+		g2.drawImage(images.get("map"), 300, 170, (int)(width * 0.8), (int)(height * 0.8), null);
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
-		// TODO Auto-generated method stub
-		
+		System.out.println(e.getX() + ", " + e.getY());
 	}
 
 	@Override
