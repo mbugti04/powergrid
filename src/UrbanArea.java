4a0224c7acf569faf314523b7c253f8ce30ee58c
malgantual <56133144+malgantual@users.noreply.github.com>
2020-02-24 16:28:11 -0600

    completed graph class for cities
    This is a test, wouldn't you agree?

--- src/UrbanArea.java
+++ src/UrbanArea.java
@@ -1,8 +1,62 @@
+import java.util.HashMap;
 
+// this is a 'graph' class for the cities
 public class UrbanArea
 {
-	public static void main(String[] args)
+	private HashMap<City, HashMap<City, Integer>> cities = new HashMap<>();
+
+	public void addCity(City c)
 	{
-//		Graph<City> = new Graph<City>(); // i don't even know
+		cities.put(c, new HashMap<City, Integer>());
+	}
+
+	public void addConnection(City source, City destination, int cost)
+	{
+		if (!cities.containsKey(source))
+			addCity(source);
+
+		if (!cities.containsKey(destination))
+			addCity(destination);
+
+		cities.get(source).put(destination, cost);
+
+		cities.get(destination).put(source, cost);
+	}
+	
+	public void removeConnection()
+	{
+		// TODO
+	}
+
+	public int getNumCities()
+	{
+		return cities.keySet().size();
+	}
+
+	public int getConnectionCount()
+	{
+		int count = 0;
+		for (City c : cities.keySet())
+		{
+			count += cities.get(c).size();
+		}
+		
+		count /= 2;
+		
+		return count;
+	}
+
+	public boolean hasCity(City c)
+	{
+		if (cities.containsKey(c))
+			return true;
+		return false;
+	}
+
+	public int hasConnection(City a, City b)
+	{
+		if (cities.get(a).containsKey(b))
+			return cities.get(a).get(b);
+		return -1;
 	}
 }
