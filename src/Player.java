253b471b017eb4aed9a1ab2e881d10fff25b9997
Yasir Yilmazcoban <34142336+yasirylmzcbn@users.noreply.github.com>
2020-03-30 11:18:39 -0500

    ooger booger

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,14 @@
-
+import java.util.*;
 public class Player {
 
+	private int money;
+	private int poweredCities;
+	
+	private boolean hasBid;
+	private boolean hasBought;
+	
+	private ArrayList<Powerplant> ownedPlants;
+	private ArrayList<City> ownedCities;
+	
+	private HashMap<Resource, Integer> resourcesStored;
 }
