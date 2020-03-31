0e28f0e13269a87a814050d9d3ef4c6055c0b178
malgantual <56133144+malgantual@users.noreply.github.com>
2020-03-31 12:31:42 -0500

    added removeCity method

--- src/UrbanArea.java
+++ src/UrbanArea.java
@@ -26,9 +26,37 @@ public class UrbanArea
 		cities.get(destination).put(source, cost);
 	}
 	
-	public void removeConnection()
+	// needed for limiting regions
+	public void removeCity(City toRemove)
 	{
-		// TODO
+		// removes key
+		if (cities.containsKey(toRemove))
+			cities.remove(toRemove);
+		
+		// removes connections from all cities
+		for (City c: cities.keySet())
+		{
+			HashMap<City, Integer> tempMap = cities.get(c);
+			if (tempMap.containsKey(toRemove))
+			{
+				tempMap.remove(toRemove);
+			}
+		}
+
+		// code for checking connections
+		/*
+		for (City c: cities.keySet())
+		{
+			System.out.println(c.getName());
+			for (City s: cities.get(c).keySet())
+			{
+				System.out.print(c.getName() + "->");
+				System.out.println(s.getName());
+			}
+		}
+		*/
+		//
+		
 	}
 
 	public int getNumCities()
@@ -63,12 +91,12 @@ public class UrbanArea
 		return -1;
 	}
 	
-	public HashMap<City, HashMap<City, Integer>> getCities()
+	public HashMap<City, HashMap<City, Integer>> getGraph()
 	{
 		return cities;
 	}
 	
-	public HashSet<City> getAllCities()
+	public HashSet<City> getCitySet()
 	{
 		HashSet<City> temp = new HashSet<City>();
 		temp.addAll(cities.keySet());
