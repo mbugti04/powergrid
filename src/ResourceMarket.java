75ac228813e2bdf17b1380773651fada614d7b39
malgantual <56133144+malgantual@users.noreply.github.com>
2020-04-02 11:56:54 -0500

    created ResourceMarket

---
+++ src/ResourceMarket.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class ResourceMarket
+{
+	private HashMap<Resource, Integer> stock;
+	
+	
+	public ResourceMarket()
+	{
+		
+	}
+	
+	private void firstTimeSetup()
+	{
+		
+	}
+	
+	public void restock()
+	{
+		
+	}
+}
