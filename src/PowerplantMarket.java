c6e569a1fb2be92bafd9b3376fae4e5efa216d1e
Vishal Sreekumar <56132735+vishal-sreekumar@users.noreply.github.com>
2020-04-02 12:02:30 -0500

    Updates with constructor and getters

--- src/PowerplantMarket.java
+++ src/PowerplantMarket.java
@@ -6,6 +6,20 @@ public class PowerplantMarket
 	private ArrayList<Powerplant> plantsAvailable;
 	private Queue<Powerplant> allPlants;
 	
+	public PowerplantMarket()
+	{
+		plantsAvailable = new ArrayList<Powerplant>();
+	}
+	
+	public ArrayList<Powerplant> getPlantsAvailable()
+	{
+		return plantsAvailable;
+	}
+	public Queue<Powerplant> getAllPlants()
+	{
+		return allPlants;
+	}
+	
 	public void restock()
 	{
 		
