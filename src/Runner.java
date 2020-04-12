a4499fcb16366765a6644582a2a25024643176b9
malgantual <56133144+malgantual@users.noreply.github.com>
2020-04-11 21:36:58 -0500

    minor testing changes

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,10 @@ public class Runner
 	public static void main(String[] args)
 	{
 		// UI game = new UI();
-		ResourceMarket x = new ResourceMarket(4);
+		ResourceMarket x = new ResourceMarket();
+		
+		System.out.println(x.currentStock);
+		x.restock();
+		System.out.println(x.currentStock);
 	}
 }
