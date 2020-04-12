54002a67c2daf2beedeed0f78ec4b778a5d2da68
malgantual <56133144+malgantual@users.noreply.github.com>
2020-04-12 17:18:17 -0500

    minor testing change

--- src/Runner.java
+++ src/Runner.java
@@ -4,10 +4,25 @@ public class Runner
 	public static void main(String[] args)
 	{
 		// UI game = new UI();
+		/*
 		ResourceMarket x = new ResourceMarket();
 		
 		System.out.println(x.currentStock);
 		x.restock();
 		System.out.println(x.currentStock);
+		*/
+		Interface f = new Interface();
+		
+		while (true)
+		{
+			f.repaint();
+			try
+			{
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 }
