c12af2203c2b1c1d1241188160c6880a32197747
malgantual <56133144+malgantual@users.noreply.github.com>
2020-05-09 19:06:03 -0500

    fix

--- src/Button.java
+++ src/Button.java
@@ -6,6 +6,7 @@ import java.awt.event.MouseEvent;
 class Button
 {
 	static int normalw = 200, normalh = 100;
+	Color color = null;
 	String name;
 	int x, y, w, h;
 	boolean on;
@@ -18,6 +19,12 @@ class Button
 		this.w = w;
 		this.h = h;
 		on = false;
+	}
+	
+	Button(String name, int x, int y, int w, int h, Color col)
+	{
+		this(name, x, y, w, h);
+		color = col;
 	}
 	
 	boolean press()
@@ -36,8 +43,10 @@ class Button
 		g2.setFont(new Font("Calibri", Font.PLAIN, 16));
 		if (!on)
 			g2.setColor(Color.DARK_GRAY);
-		else
+		else if (on)
 			g2.setColor(new Color(12, 120, 123));
+		if (color != null)
+			g2.setColor(color);
 		g2.fillRect(x, y, w, h);
 		g2.setColor(Color.WHITE);
 		g2.drawString(name, x + w / 10, y + h / 2);
