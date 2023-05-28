d2619b3f55ea3beebee1c58e3d3b028de619445f
mbugti04 <mustafammbg@gmail.com>
2023-05-28 11:20:41 -0500

    Moved frame to Runner; Switched to invokeLater

--- src/Runner.java
+++ src/Runner.java
@@ -1,32 +1,39 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class Runner
-{
-	public static void main(String[] args)
-	{
-		runGame();
-//		bidTest();
-	}
-	public static void bidTest()
-	{
-		GameState e = new GameState();
-//		e.bid(); bid needs a parameter now (bid(Powerplant plantChosen))
-	}
-	public static void runGame()
-	{
-		Interface f = new Interface();
-		
-		while (true)
-		{
-			f.repaint();
-//			f.info();
-			try
-			{
-				Thread.sleep(1000/30);
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Runner {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame frame = new JFrame("Simple Panel");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				Interface panel = new Interface();
+				frame.add(panel);
+				frame.setSize(panel.getWidth(), panel.getHeight());
+				try {
+					frame.setIconImage(ImageIO.read(new File("assets/images/icon.png")));
+				} catch (IOException e) {
+					System.out.println("Could not load icon!");
+				}
+
+				// frame.pack();
+				// frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				// frame.setUndecorated(true);
+
+				try {
+					frame.setIconImage(ImageIO.read(new File("assets/images/icon.png")));
+				} catch (IOException e) {
+					System.out.println("Could not load icon!");
+				}
+				frame.setVisible(true);
 			}
-			catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
-		}
+		});
 	}
 }
